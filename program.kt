5aa752f6ac2e64f7d7173a0dd7188ad1499edd07
FATIQ HUSSNAIN <96644936+fatiq123@users.noreply.github.com>
2023-07-07 00:50:31 -0700

    Update program.kt
    created a function to return the sum of two numbers.

--- program.kt
+++ program.kt
@@ -3,10 +3,18 @@ fun main(){
   
   print("Hello World")
   hello  // do the same work as above line does
+
+  sum(5,5)  // outputs 10 
   
 }
 
 private fun hello() : String {
   
      return "Hello World"
+}
+
+private fun sum(var a: Int, var b: Int): Int {
+
+  return a + b
+  
 }
