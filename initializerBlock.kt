5fa4426863df4779e4b703356f26dba848d5b07f
FATIQ HUSSNAIN <96644936+fatiq123@users.noreply.github.com>
2023-07-27 10:53:09 -0700

    Create initializerBlock.kt

---
+++ initializerBlock.kt
@@ -0,0 +1,14 @@
+class animal{
+
+  init{
+    animalInfo("lion", "forest)
+  }
+
+  fun animalInfo(name: String, origin: String) {
+
+    println("Name of animal is: $name")
+    println("Origin of animal is: $origin")
+    
+  }
+  
+}
